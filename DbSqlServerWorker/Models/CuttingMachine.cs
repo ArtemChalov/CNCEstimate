5e8c4956dc6b4c72b96d9d9b521cdef4c9cd150b
ArtemChalov <chalov81@mail.ru>
2019-02-12 22:11:35 +0500

    Added DbSqlServerWorker project.

---
+++ DbSqlServerWorker/Models/CuttingMachine.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbSqlServerWorker.Models
+{
+    
+        public class CuttingMachine
+        {
+            public byte CuttingMachinesId { get; set; }
+            public string TypeTitle { get; set; }
+            public string ShortTitle { get; set; }
+        }
+}
