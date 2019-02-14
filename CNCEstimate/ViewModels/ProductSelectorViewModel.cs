3a34e3581775f0924afd64fdc9aa1462ff414c28
ArtemChalov <chalov81@mail.ru>
2019-02-14 23:20:11 +0500

    ProductSelector on air.

--- CNCEstimate/ViewModels/ProductSelectorViewModel.cs
+++ CNCEstimate/ViewModels/ProductSelectorViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
 using Caliburn.Micro;
 using DbSqlServerWorker.Models;
 
@@ -7,10 +9,14 @@ namespace CNCEstimate.ViewModels
     public class ProductSelectorViewModel : Screen
     {
         private Material _material;
+        private bool _stComboListVisible;
+        private List<string> _standartProductList;
+        private string _selectedStandartProduct;
 
         public ProductSelectorViewModel(Material material)
         {
             _material = material;
+            StComboListVisible = false;
         }
 
         public Material Material
@@ -23,13 +29,56 @@ namespace CNCEstimate.ViewModels
             }
         }
 
+        public bool StComboListVisible
+        {
+            get { return _stComboListVisible; }
+            set
+            {
+                _stComboListVisible = value;
+                NotifyOfPropertyChange(nameof(StComboListVisible));
+            }
+        }
+
+        public List<string> StandartProductList
+        {
+            get { return _standartProductList; }
+            set
+            {
+                _standartProductList = value;
+                NotifyOfPropertyChange(nameof(StandartProductList));
+            }
+        }
+
+        public string SelectedStandartProduct
+        {
+            get { return _selectedStandartProduct; }
+            set
+            {
+                _selectedStandartProduct = value;
+                if (value != null)
+                    OnStandartProductSelected(value);
+            }
+        }
+
         private void OnMaterialChanged(Material material)
         {
         }
 
-        public void CreateProduct()
+        public void CreateStandartProduct()
         {
-            MessageBox.Show($"I'm from {Material.Title}");
+            StandartProductList = new List<string> { "Косынка", "Пластина" };
+            StComboListVisible = true;
+        }
+
+        public void CloseProduct()
+        {
+            StandartProductList = null;
+            StComboListVisible = false;
+        }
+
+        private void OnStandartProductSelected(string stProductName)
+        {
+            MessageBox.Show(stProductName);
         }
     }
 }
