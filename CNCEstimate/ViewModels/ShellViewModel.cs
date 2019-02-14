055f391528153ba1dfed6458c98091b44f6695df
ArtemChalov <chalov81@mail.ru>
2019-02-14 13:31:26 +0500

    Refactored ShellView.

--- CNCEstimate/ViewModels/ShellViewModel.cs
+++ CNCEstimate/ViewModels/ShellViewModel.cs
@@ -1,22 +1,15 @@
 ﻿using Caliburn.Micro;
 using CNCEstimate.Dialogs;
 using CNCEstimate.Factories;
-using DbSqlServerWorker.DataLoaders;
 using DbSqlServerWorker.Models;
-using System.Collections.Generic;
 using System.Windows;
 
 namespace CNCEstimate.ViewModels
 {
     public class ShellViewModel : Conductor<object>
     {
-        private List<MaterialGroup> _material;
-        private MaterialGroup _selectedMater;
-
         public ShellViewModel()
         {
-            MaterialGroups = MaterialLoader.FetchMaterialGroups();
-            IsVisibleMaterialBox = false;
             AppStore.OnMachineSelected += AppStore_OnMachineSelected;
             AppStore.MainWindow = (Window)this.GetView();
         }
@@ -25,50 +18,15 @@ namespace CNCEstimate.ViewModels
         {
             if (machine != null)
             {
-                IsVisibleMaterialBox = true;
                 ActivateItem(new MachinViewFactory(machine.TypeTitle).GetView());
             }
-            NotifyOfPropertyChange(nameof(IsVisibleMaterialBox));
-        }
-
-        public bool IsVisibleMaterialBox { get; private set; }
-
-        public List<MaterialGroup> MaterialGroups
-        {
-            get { return _material; }
-            set { _material = value; }
-        }
-
-        public string MaterialTitle { get; set; }
-
-        public MaterialGroup SelectedMater
-        {
-            get { return _selectedMater; }
-            set
-            {
-                _selectedMater = value;
-                if (value != null)
-                {
-                    MaterialTitle = value.GroupTitle;
-                    NotifyOfPropertyChange(nameof(MaterialTitle));
-                }
-            }
-        }
-
-        public void SelectMaterial()
-        {
-            ChooseMaterialDialog dialog = new ChooseMaterialDialog()
-            {
-                Owner = (Window)this.GetView()
-            };
-            dialog.ShowDialog();
         }
 
         public void ChoseCutType()
         {
             ChooseCutMachineDialog dialog = new ChooseCutMachineDialog()
             {
-                Owner = (Window)this.GetView()
+                Owner = AppStore.MainWindow
             };
             dialog.SetCurrentMachine(AppStore.SelectedMachine);
 
