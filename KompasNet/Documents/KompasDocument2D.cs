e56d3540772d1fc877444c719348145174e2de34
ArtemChalov <chalov81@mail.ru>
2019-02-19 21:52:41 +0500

    Document

---
+++ KompasNet/Documents/KompasDocument2D.cs
@@ -0,0 +1,45 @@
+﻿using KAPITypes;
+using Kompas6API5;
+using Kompas6Constants;
+using KompasNet.Models;
+
+namespace KompasNet.Documents
+{
+    public class KompasDocument2D
+    {
+        public void CreateDocument(string fileName, MainStamp mainStamp)
+        {
+            KompasObjectFactory.Open();
+
+            if (KompasObjectFactory.Kompas != null)
+            {
+                KompasObject Kompas = KompasObjectFactory.Kompas;
+                Kompas.Visible = true;
+
+                DocumentParam documentParam;
+                documentParam = (DocumentParam)Kompas.GetParamStruct((short)StructType2DEnum.ko_DocumentParam);
+                documentParam.Init();
+                documentParam.type = (short)DocType.lt_DocSheetStandart;
+                documentParam.fileName = fileName;
+
+                SheetPar sheetPar;
+                sheetPar = (SheetPar)documentParam.GetLayoutParam();
+                sheetPar.layoutName = @"C:\Program Files\ASCON\KOMPAS-3D v17\Sys\GRAPHIC.LYT"; 
+                sheetPar.shtType = 1;  //Тип документа
+
+                //Подготавливаем параметры листа
+                StandartSheet standartSheet;
+                standartSheet = (StandartSheet)sheetPar.GetSheetParam();
+                standartSheet.direct = false; //надпись вдоль короткой стороны
+                standartSheet.format = 4;     //А4
+                standartSheet.multiply = 1;   //кратность
+
+                Document2D document2D;
+                document2D = (Document2D)Kompas.Document2D();
+                document2D.ksCreateDocument(documentParam);
+
+                new CreateStamp().Create(document2D, mainStamp);
+            }
+        }
+    }
+}
